bb73d203b24d3a839bc1b4a2b8b949c48633ce8d
peedrofariaa <pedrofariagaldino@gmail.com>
2023-10-27 17:48:35 -0300

    2

--- src/app/dashboard/teacher/page.tsx
+++ src/app/dashboard/teacher/page.tsx
@@ -1,5 +1,28 @@
 const TeacherDashboard = () => {
-  return <p>Dashboard Page</p>;
+  return (
+    <body>
+      <header className="flex justify-between mb-8 h-28 pt-8 pb-8 px-10">
+        <div className="flex flex-col items-start">
+          <h1 className="text-xs font-normal">ESCOLA</h1>
+          <h1 className="text-2xl font-normal">Mundial</h1>
+        </div>
+        <div className="flex items-center">
+          <button className="bg-blue-800 text-white w-20 h-14 text-base rounded-md">
+            Perfil
+          </button>
+        </div>
+      </header>
+      <main>
+        <div className="flex justify-between mt-10">
+          <h1>Meus Alunos</h1>
+          <input className="" type="search" placeholder="Pesquisar alunos" />
+        </div>
+        <div>
+
+        </div>
+      </main>
+    </body>
+  );
 };
 
 export default TeacherDashboard;
