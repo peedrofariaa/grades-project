c51b84df638aac5d8234d6bba6cbd64e932c3567
peedrofariaa <pedrofariagaldino@gmail.com>
2023-10-31 18:22:19 -0300

    logo image added

--- src/app/dashboard/teacher/page.tsx
+++ src/app/dashboard/teacher/page.tsx
@@ -1,12 +1,13 @@
 import DashboardProfessor from "@/components/TeacherDashboard";
+import logo from '@/assets/logo.png'
+import Image from 'next/image'
 
 const TeacherDashboard = () => {
   return (
     <body>
       <header className="flex justify-between mb-8 h-28 pt-8 pb-8 px-10">
         <div className="flex flex-col items-start">
-          <h1 className="text-xs font-normal">ESCOLA</h1>
-          <h1 className="text-2xl font-normal">Mundial</h1>
+          <Image src={logo} alt="logo"></Image>
         </div>
         <div className="flex items-center">
           <button className="bg-blue-700 text-white w-20 h-14 text-lg font-semibold rounded-md">
