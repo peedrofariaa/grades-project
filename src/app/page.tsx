49eee6ef87e9ce14024048d188a5fcba00481139
peedrofariaa <pedrofariagaldino@gmail.com>
2023-10-31 00:05:49 -0300

    Finished home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,8 +5,8 @@ export default function Home() {
       <main className="flex flex-col items-center justify-start pt-10">
         <h1 className="font-bold text-3xl mb-12">Sistema de Consulta de Notas</h1>
         <div className="flex flex-col">
-          <Link className="mb-5" href="/login/student">- Login como <strong>Aluno</strong></Link>
-          <Link href="/login/teacher">- Login como <strong>Professor</strong></Link>
+          <Link className="mb-5" href="/login/student">- Entrar como <strong>Aluno</strong></Link>
+          <Link href="/login/teacher">- Entrar como <strong>Professor</strong></Link>
         </div>
       </main>
   );
