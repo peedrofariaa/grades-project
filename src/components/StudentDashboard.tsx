a5e0170dcb990ac825609645f9c6239332c901b2
peedrofariaa <pedrofariagaldino@gmail.com>
2023-11-01 17:50:12 -0300

    Dashboards pages fixed

--- src/components/StudentDashboard.tsx
+++ src/components/StudentDashboard.tsx
@@ -1,63 +1,80 @@
 "use client";
-import { useEffect, useState } from "react";
-
-type StudentScore = {
-  n1: number;
-  n2: number;
-  n3: number;
-  n4: number;
-  average: number;
-  situation: string;
-};
+import { AuthContext } from "@/context/AuthContext";
+import { useApi } from "@/services/api";
+import { useContext, useEffect, useState } from "react";
 
 const DashboardAluno = () => {
-  const [studentScore, setStudentScore] = useState(null);
-  const [isLoading, setIsLoading] = useState(true);
+  const api = useApi();
+  const auth = useContext(AuthContext);
+  const [rows, setRows] = useState<Array<object>>();
+//  const [isLoading, setIsLoading] = useState(true);
 
   useEffect(() => {
-    const fetchStudentScore = async () => {
-      try {
-        const res = await fetch("https://test-dev.tikal.tech/aluno/score", {
-          headers: {
-            Authorization: "Bearer xxx",
-          },
+    const handleCallStudent = async () => {
+      if (auth?.user) {
+        const studentRes: any = await api.callStudent(auth.user?.token);
+        studentRes?.score.map((score: any) => {
+          let n1 = score?.n1.toFixed(2);
+          let n2 = score?.n2.toFixed(2);
+          let n3 = score?.n3.toFixed(2);
+          let n4 = score?.n4.toFixed(2);
+          let average = score?.average.toFixed(2);
+          let situation = score?.situation;
+     
+          return setRows([
+            ...(rows ?? []),
+            {
+              n1,
+              n2,
+              n3,
+              n4,
+              average,
+              situation,
+            },
+          ]);
         });
-        const data = await res.json();
-        setStudentScore(data);
-        setIsLoading(false);
-      } catch (error) {
-        console.error(error);
       }
     };
-    fetchStudentScore();
+    handleCallStudent();
   }, []);
 
-  if (isLoading) {
-    return (
-      <div className="mt-10 flex justify-center font-bold text-2xl">
-        Loading...
-      </div>
-    );
-  }
+//  if (isLoading) {
+//    return (
+//      <div className="mt-10 flex justify-center font-bold text-2xl">
+//        Loading...
+//      </div>
+//    );
+//  }
 
   return (
     <table className="ml-20 mt-4">
-      <tr className="bg-[#F3F4F6] h-14 font-medium">
-        <th className="pr-28 pl-4">Av. 1</th>
-        <th className="pr-28">Av. 2</th>
-        <th className="pr-28">Av. 3</th>
-        <th className="pr-28">Av. 4</th>
-        <th className="pr-28">Média Final</th>
-        <th className="pr-24">Situação</th>
-      </tr>
-      <tr className="bg-white">           
-        <td className="pl-4">Fábio</td>
-        <td>Flavio</td>
-        <td>Flavio</td>
-        <td>Flavio</td>
-        <td>Flavio</td>
-        <td>Flavio</td>
-      </tr>
+      <thead>
+        <tr className="bg-[#F3F4F6] h-14 font-medium">
+          <th className="pr-28 pl-4">Av. 1</th>
+          <th className="pr-28">Av. 2</th>
+          <th className="pr-28">Av. 3</th>
+          <th className="pr-28">Av. 4</th>
+          <th className="pr-28">Média Final</th>
+          <th className="pr-24">Situação</th>
+        </tr>
+      </thead>
+      <tbody>
+        {rows?.map((row: any, index) => (
+          <tr key={index} className="bg-white">
+            <td className="pl-4">{row?.n1}</td>
+            <td>{row?.n2}</td>
+            <td>{row?.n3}</td>
+            <td>{row?.n4}</td>
+            <td>{row?.average}</td>
+            <td>
+              <div>
+                {row?.situation === "aprovado" ? `aproved` : `failed`}
+                {row?.situation}
+              </div>
+            </td>
+          </tr>
+        ))}
+      </tbody>
     </table>
   );
 };
--- src/components/TeacherDashboard.tsx
+++ src/components/TeacherDashboard.tsx
@@ -1,66 +1,98 @@
 "use client";
-import { useEffect, useState } from "react";
-
-type StudentData = {
-  updatedAt: string;
-  firstName: string;
-  lastName: string;
-  n1: number;
-  n2: number;
-  n3: number;
-  n4: number;
-  average: number;
-  situation: string;
-};
+import { AuthContext } from "@/context/AuthContext";
+import { useContext, useEffect, useState } from "react";
+import { useApi } from "@/services/api";
+import { format } from "date-fns";
 
 const DashboardProfessor = () => {
-  const [studentData, setStudentData] = useState([]);
-  const [isLoading, setIsLoading] = useState(true);
+  const api = useApi();
+  const auth = useContext(AuthContext);
+  const [rows, setRows] = useState<Array<object>>();
+//  const [isLoading, setIsLoading] = useState(true);
 
   useEffect(() => {
-    const fetchStudentData = async () => {
-      try {
-        const res = await fetch("https://test-dev.tikal.tech/adm/student", {
-          headers: {
-            Authorization: "Bearer xxx",
-          },
-        });
-        const data = await res.json();
-        setStudentData(data);
-        setIsLoading(false);
-      } catch (error) {
-        console.error(error);
+    const handleCallTeacher = async () => {
+      if (auth?.user) {
+        const teacherRes: any = await api.callTeacher(auth.user?.token);
+        for (let i: number = 0; teacherRes.length >= i; i++) {
+          let name = teacherRes[i]?.firstName + "" + teacherRes[i]?.lastName;
+          teacherRes[i]?.scores.map((score: any) => {
+            let updated = format(new Date(score?.updatedAt), "dd-MM-yyyy");
+            let updatedHour = format(new Date(score?.updatedAt), "hh:mm");
+            let n1 = score?.n1.toFixed(2);
+            let n2 = score?.n2.toFixed(2);
+            let n3 = score?.n3.toFixed(2);
+            let n4 = score?.n4.toFixed(2);
+            let average = score?.average.toFixed(2);
+            let situation = score?.score.situation;
+
+            return setRows([
+              ...(rows ?? []),
+              {
+                name,
+                updated,
+                updatedHour,
+                n1,
+                n2,
+                n3,
+                n4,
+                average,
+                situation,
+              },
+            ]);
+          });
+        }
       }
     };
-    fetchStudentData();
+    handleCallTeacher();
   }, []);
 
-  if (isLoading) {
-    return <div className="mt-10 flex justify-center font-bold text-2xl">Loading...</div>;
-  }
+//  if (isLoading) {
+//    return (
+//      <div className="mt-10 flex justify-center font-bold text-2xl">
+//        Loading...
+//      </div>
+//    );
+//  }
 
   return (
     <table className="ml-20 mt-4">
-      <tr className="bg-[#F3F4F6] h-14 font-medium">
-        <th className="pr-12 pl-4">Nome do Aluno</th>
-        <th className="pr-16">Atualização</th>
-        <th className="pr-28">Av. 1</th>
-        <th className="pr-28">Av. 2</th>
-        <th className="pr-28">Av. 3</th>
-        <th className="pr-28">Av. 4</th>
-        <th className="pr-16">Média Final</th>
-        <th className="pr-24">Situação</th>
-      </tr>
-      <tr className="bg-white">           
-        <td className="pl-4">Fábio</td>
-        <td>Fábio</td>
-        <td>Flavio</td>
-        <td>Flavio</td>
-        <td>Flavio</td>
-        <td>Flavio</td>
-        <td>Flavio</td>
-        <td>Flavio</td>
-      </tr>
+      <thead>
+        <tr className="bg-[#F3F4F6] h-14 font-medium">
+          <th className="">Nome do Aluno</th>
+          <th className="">Atualização</th>
+          <th className="pr-28 pl-4">Av. 1</th>
+          <th className="pr-28">Av. 2</th>
+          <th className="pr-28">Av. 3</th>
+          <th className="pr-28">Av. 4</th>
+          <th className="pr-28">Média Final</th>
+          <th className="pr-24">Situação</th>
+        </tr>
+      </thead>
+      <tbody>
+        {rows?.map((row: any, index: any) => (
+          <tr key={index}>
+            <td>{row?.name}</td>
+            <td>
+              <div>
+                <p>{row?.updated}</p>
+                <span>{row?.updatedHour}</span>
+              </div>
+            </td>
+            <td>{row?.n1}</td>
+            <td>{row?.n2}</td>
+            <td>{row?.n3}</td>
+            <td>{row?.n4}</td>
+            <td>{row?.average}</td>
+            <td>
+              <div>
+                {row?.situation === "aprovado" ? `aproved` : `failed`}
+                {row?.situation}
+              </div>
+            </td>
+          </tr>
+        ))}
+      </tbody>
     </table>
   );
 };
