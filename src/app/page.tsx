f0422d5352450da71da16366b00488b60fd1179a
peedrofariaa <pedrofariagaldino@gmail.com>
2023-11-07 17:45:21 -0300

    fix: design tela HOME

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ import Link from "next/link";
 export default function Home() {
   return (
       <main className="flex flex-col items-center justify-start pt-10">
-        <h1 className="font-bold text-3xl mb-12">Sistema de Consulta de Notas</h1>
+        <h1 className="font-bold text-2xl mb-20">Sistema de Consulta de Notas</h1>
         <div className="flex flex-col text-xl">
           <Link className="mb-5" href="/login/student">- Entrar como <strong>Aluno</strong></Link>
           <Link href="/login/teacher">- Entrar como <strong>Professor</strong></Link>
