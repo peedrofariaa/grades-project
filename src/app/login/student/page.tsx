27e3e50c0a32c2c74998521640d435d0a0756ecb
peedrofariaa <pedrofariagaldino@gmail.com>
2023-11-07 17:45:46 -0300

    fix: design telas login

--- src/app/login/student/page.tsx
+++ src/app/login/student/page.tsx
@@ -24,28 +24,28 @@ const StudentLogin: React.FC = () => {
   return (
     <main className="flex flex-col items-center">
       <div className="pt-10">
-        <h1 className="text-3xl font-bold mb-8">Bem vindo Aluno</h1>
+        <h1 className="text-2xl font-bold mb-8">Bem vindo Aluno</h1>
       </div>
       <div className="flex flex-col">
         <input
           type="email"
           value={email}
           onChange={(e) => setEmail(e?.target?.value)}
-          className="w-80 h-16 border-none text-lg bg-white rounded-2xl outline-none mb-5 pl-5 placeholder:text-black"
+          className="w-80 h-16 border-none text-lg bg-white rounded outline-none mb-5 pl-5 placeholder:text-black"
           placeholder="Email"
         ></input>
         <input
           type="password"
           value={password}
           onChange={(e) => setPassword(e?.target?.value)}
-          className="w-80 h-16 border-none text-lg bg-white rounded-2xl outline-none mb-5 pl-5 placeholder:text-black"
+          className="w-80 h-16 border-none text-lg bg-white rounded outline-none mb-5 pl-5 placeholder:text-black"
           placeholder="Senha"
         ></input>
       </div>
       <div>
         <button
           onClick={handleStudentLogin}
-          className="bg-blue-700 w-80 h-14 rounded-2xl text-white font-bold text-xl hover:opacity-80"
+          className="bg-blue-700 w-80 h-14 rounded-md text-white font-bold text-xl hover:opacity-80"
         >
           Entrar
         </button>
